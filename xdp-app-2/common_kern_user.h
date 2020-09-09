d2001b65a2dd9c4fe383a761769168133039b139
bibek shrestha <sbibek@hotmail.com>
2020-09-09 10:49:27 -0700

    new d

---
+++ xdp-app-2/common_kern_user.h
@@ -0,0 +1,79 @@
+#ifndef __COMMON_KERN_USER_H
+#define __COMMON_KERN_USER_H
+
+#define TOTAL_KEYS 0
+#define MAX_ENTRIES_TOTAL_KEYS 1
+#define MAX_ENTRIES_FLOW_KEYS 10000
+#define MAX_ENTRIES_FLOWS 10000
+
+struct datarec
+{
+	__u64 rx_packets;
+	__u64 rx_bytes;
+};
+
+struct packet_metadata
+{
+	__u32 key;
+	__u16 ethernet_protocol;
+	__u32 ip_src;
+	__u32 ip_dst;
+	__u8 ip_ttl;
+	__u8 ip_protocol;
+
+	/* populated for both TCP and UDP */
+	__be16 src_p;
+	__be16 dst_p;
+	__be32 length; // payload length
+
+	/* this is populated if TCP Only */
+	__be32 acknowledge;
+	__be32 sequence;
+	__be16 window;
+	__be16 urg_ptr;
+	__u16 cwr,
+		ece,
+		urg,
+		ack,
+		psh,
+		rst,
+		syn,
+		fin;
+};
+
+struct total_keys {
+	__u64 total_keys;
+};
+
+struct flow_key_info
+{
+	__u32 key;
+	__u32 ip_src;
+	__u32 ip_dst;
+	__be16 src_p;
+	__be16 dst_p;
+	__u8 ip_protocol;
+};
+
+struct flows_info {
+	__u64 timestamp;
+	unsigned long checksum;
+	__u64 totalPackets,
+			totalBytes,
+			totalRxBytes,
+			totalTxBytes,
+			totalTtl,
+			totalEce,
+			totalUrg,
+			totalAck,
+			totalPsh,
+			totalRst,
+			totalSyn,
+			totalFin;
+};
+
+#ifndef XDP_ACTION_MAX
+#define XDP_ACTION_MAX (XDP_REDIRECT + 1)
+#endif
+
+#endif
